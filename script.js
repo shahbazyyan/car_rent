7a830ce6f9a96c7dd59156ee3a80560f040a9dfc
David Shahbazyan Mushegh <david.shahbazyan.m@totogaming.am>
2023-11-03 14:00:45 +0400

    Created cartitems with OOP, added some details and fixed some styles

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//menu
 const menu = document.querySelector("#menu-icon");
 const navbar = document.querySelector(".navbar");
 
@@ -10,6 +12,9 @@ window.onscroll = () => {
   menu.classList.remove("fa-xmark");
   navbar.classList.remove("active");
 };
+
+
+//scroll effects
 
 const sr = ScrollReveal({
   distance: '60px',
@@ -48,16 +53,17 @@ sr.reveal(".about-container .box", {
   origin: 'top', 
 });
 
+sr.reveal(".about-container .about-img img", {
+  delay: 700,
+  origin: 'left', 
+});
+
 sr.reveal(".reviwes-container", {
   delay: 600,
   origin: 'top', 
 });
 
-sr.reveal(".newsletter .box", {
-  delay: 400,
-  origin: 'bottom', 
-});
-
+//slider
 
 let swiper = new Swiper(".vehicles-slider", {
   slidesPerView: 1,
@@ -86,5 +92,124 @@ let swiper = new Swiper(".vehicles-slider", {
   },
 });
 
+// caritems with oop
+
+class CarsCarts {
+  constructor (img,alt,year,car,price,rentTime,parentSelector) {
+    this.img = img;
+    this.alt = alt;
+    this.year = year;
+    this.car = car;
+    this.price = price;
+    this.rentTime = rentTime;
+    this.parent = document.querySelector(parentSelector);
+  };
+
+  render () {
+    const {img,alt,year,car,price,rentTime,parent} = this;
+    const elem = document.createElement("div");
+    elem.classList.add("box");
+    elem.innerHTML = `
+              <div class="box-img">
+                    <img src=${img} alt=${alt}>
+                </div>
+                <p>${year}</p>
+                <h3>${car}</h3>
+                <h2> ${price} <span>${rentTime}</h2>
+                <a href="#" class="btn">Rent Now</a>
+    `;
+    parent.append(elem);
+  };
+};
+
+  new CarsCarts (
+    "img/ferrari.png",
+    "car",
+    "2019",
+    "Ferrari F8 Tributo",
+    "$14.300 / $630",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/mercedes.png",
+    "car",
+    "2020",
+    "Mercedes Benz C300 Convertible",
+    "$6.700 / $420",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/bmw1.png",
+    "car",
+    "2020",
+    "Voodoo Blue BMW M3 ",
+    "$6.550 / $255",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/kia.png",
+    "car",
+    "2023",
+    "Kia Optima",
+    "$4.800 / $290",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/mazda.png",
+    "car",
+    "2023",
+    "Mazda CX-30",
+    "$3.800 / $320",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/astonmartin.png",
+    "car",
+    "2024",
+    "Aston Martin DB12 revealed ",
+    "$18.650$ / $920",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/bugatti.png",
+    "car",
+    "2022",
+    "Bugatti Chiron Sport",
+    "$18.700 / $820",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/hyundai.png",
+    "car",
+    "2023",
+    "Hyundai Santa Fe",
+    "$9.000 / $370",
+    "month/day",
+    ".services-container"
+  ).render();
+
+  new CarsCarts (
+    "img/tesla.png",
+    "car",
+    "2022",
+    "Tesla Model 3",
+    "$21.300 / $1.000",
+    "month/day",
+    ".services-container"
+  ).render();
 
 
